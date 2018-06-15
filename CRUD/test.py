fc55fbb5beed09cbcf1cbcffc3159d4d67ff96d4
105598025 <jack888835@gmail.com>
2018-06-15 16:28:43 +0800

    123

--- CRUD/test.py
+++ CRUD/test.py
@@ -9,8 +9,3 @@ desired_caps['appActivity'] = 'DialtactsActivity'
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
 
-
-driver.find_element_by_id('com.google.android.dialer:id/search_box_start_search').click()
-search_box = driver.find_element_by_id('com.google.android.dialer:id/search_view')
-search_box.click()
-search_box.send_keys('hello toby')
