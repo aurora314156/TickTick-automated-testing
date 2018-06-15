cbf87c22e529f5ca6ca70bb4087c9403535c07be
neil399399 <neil399399@gmail.com>
2018-06-15 21:27:23 +0800

    done tc01~tc05

--- CRUD/test.py
+++ CRUD/test.py
@@ -6,13 +6,41 @@ desired_caps['platformVersion'] = '8.0'
 desired_caps['deviceName'] = 'Android Emulator'
 desired_caps['appPackage'] = 'com.ticktick.task'
 desired_caps['appActivity'] = '.activity.MeTaskActivity'
-
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-
-
-# test Task General.
+## skip_tutorial.
 driver.find_element_by_id('com.ticktick.task:id/skip_tutorial').click()
+
+
+# TC-01
 ## create new task.
 click_button = driver.find_element_by_id('com.ticktick.task:id/add_task_btn').click()
-click_title = driver.find_element_by_id('com.ticktick.task:id/quick_add_title').click()
-click_title.send_keys('hello')
+click_title = driver.find_element_by_id('com.ticktick.task:id/quick_add_title')
+click_title.click()
+click_title.send_keys('task_1')
+save_btn = driver.find_element_by_id('com.ticktick.task:id/save_btn').click()
+
+
+# TC-02
+
+
+# TC-03
+## edit task_1.
+driver.find_element_by_id('com.ticktick.task:id/list').click()
+driver.find_element_by_xpath("//android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.view.View[@index='0']").click()
+edit_task_1 = driver.find_element_by_id('com.ticktick.task:id/edit_text')
+edit_task_1.click()
+edit_task_1.send_keys('task_1_edited')
+driver.find_element_by_xpath("//android.widget.ImageButton").click()
+
+# TC-04
+## delete task.
+# driver.find_element_by_xpath("//android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.view.View[@index='0']").click()
+# tool_bar = driver.find_element_by_id('com.ticktick.task:id/toolbar').click()
+# driver.find_element_by_xpath("//android.support.v7.widget.LinearLayoutCompat/android.widget.ImageView[@index='0']").click()
+# driver.find_element_by_xpath("//android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[@index='9']").click()
+
+# TC-05
+## done task.
+driver.find_element_by_xpath("//android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.view.View[@index='0']").click()
+Checkbox = driver.find_element_by_id('com.ticktick.task:id/task_checkbox').click()
+driver.find_element_by_xpath("//android.widget.ImageButton").click()
