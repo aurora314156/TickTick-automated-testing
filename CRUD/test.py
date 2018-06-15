62434457b15897c5c6bacc05260bcb6bfc04d402
neil399399 <neil399399@gmail.com>
2018-06-15 17:22:52 +0800

    test

--- CRUD/test.py
+++ CRUD/test.py
@@ -4,13 +4,16 @@ desired_caps = {}
 desired_caps['platformName'] = 'Android'
 desired_caps['platformVersion'] = '8.0'
 desired_caps['deviceName'] = 'Android Emulator'
-desired_caps['appPackage'] = 'com.google.android.dialer'
-desired_caps['appActivity'] = 'DialtactsActivity'
+desired_caps['appPackage'] = 'com.ticktick.task'
+desired_caps['appActivity'] = 'activity.DispatchActivity'
+desired_caps['skipUnlock'] = 'true'
+
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
 
-# test.
-driver.find_element_by_id('com.google.android.dialer:id/search_box_start_search').click()
-search_box = driver.find_element_by_id('com.google.android.dialer:id/search_view')
-search_box.click()
-search_box.send_keys('hello toby')
+# test Task General.
+driver.find_element_by_id('com.ticktick.task:id/skip_tutorial').click()
+## create new task.
+click_button = driver.find_element_by_id('com.ticktick.task:id/add_task_btn').click()
+click_title = driver.find_element_by_id('com.ticktick.task:id/quick_add_title').click()
+click_title.send_keys('hello')
