1fa280fee3272de3ff39d64fdc44f6a3f7452030
neil399399 <neil399399@gmail.com>
2018-06-20 00:36:14 +0800

    done TC-01~05

--- CRUD/TC-345.py
+++ CRUD/TC-345.py
@@ -1,5 +1,4 @@
 from appium import webdriver
-from appium.webdriver.common.touch_action import TouchAction
 
 desired_caps = {}
 desired_caps['platformName'] = 'Android'
@@ -9,19 +8,17 @@ desired_caps['appPackage'] = 'com.ticktick.task'
 desired_caps['appActivity'] = '.activity.MeTaskActivity'
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 ## skip_tutorial.
-driver.find_element_by_id('com.ticktick.task:id/skip_tutorial').click()
-
-## click calendar.
-driver.find_element_by_id('com.ticktick.task:id/navigation_calendar_id').click()
-
-# TC-06
 try:
-## create new task in calendar.
-## create new task.
-    driver.find_element_by_id('com.ticktick.task:id/add_task_btn').click()
+    driver.find_element_by_id('com.ticktick.task:id/skip_tutorial').click()
+except:
+    print('initial error.')
+
+# TC-03
+try:
+    click_button = driver.find_element_by_id('com.ticktick.task:id/add_task_btn').click()
     click_title = driver.find_element_by_id('com.ticktick.task:id/quick_add_title')
     click_title.click()
-    click_title.send_keys('task_1_calendar')
+    click_title.send_keys('task_1')
 
     ## pick up time and date.
     driver.find_element_by_id('com.ticktick.task:id/pick_up_time').click()
@@ -31,30 +28,26 @@ try:
     driver.find_element_by_id('com.ticktick.task:id/priority_toggle').click()
     driver.find_element_by_xpath('//android.widget.RelativeLayout/android.widget.RadioButton').click()
 
+    # ## add tag.
+    # driver.find_element_by_id('com.ticktick.task:id/tag_toggle').click()
+    # add_tag = driver.find_element_by_id('com.ticktick.task:id/quick_add_title').send_keys('tag')
+   
     ## choose inbox.
     driver.find_element_by_id('com.ticktick.task:id/project_layout').click()
     driver.find_element_by_xpath("//android.widget.RelativeLayout/android.widget.TextView[@text='Inbox']").click()
+    # create_taskList = driver.find_element_by_id('com.ticktick.task:id/text_create_tasklist')
+    # create_taskList.send_keys('work')
+    # driver.find_element_by_id('android:id/button1').click()  # button ok.
 
+    # save task and show .
     save_btn = driver.find_element_by_id('com.ticktick.task:id/save_btn').click()
-except:
-    print('TC-06 error.')
+    # driver.find_element_by_xpath('//android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ImageButton').click()
+    # driver.find_element_by_xpath("//android.widget.RelativeLayout/android.widget.TextView[@text='work']").click()
 
-# TC-07
-try:
-## edit task in calendar.
+
+    ## edit task_name.
     driver.find_element_by_id('com.ticktick.task:id/list').click()
-    driver.find_element_by_xpath("//android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v7.widget.RecyclerView/android.view.View[@index='2']").click()
-    ## edit date.
-    driver.find_element_by_id('com.ticktick.task:id/task_date_text').click()
-    # touch.
-    # view_pager = driver.find_element_by_id('com.ticktick.task:id/viewpager')
-    loc = driver.find_element_by_android_uiautomator('com.ticktick.task:id/viewpager').location
-    size = driver.find_element_by_android_uiautomator('com.ticktick.task:id/viewpager').size
-    print(loc,size)
-    TouchAction(driver).tap(view_pager,None,1119).release()
-    driver.find_element_by_xpath("//android.widget.ImageButton").click()
-
-    ## edit task name.
+    driver.find_element_by_xpath("//android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.view.View[@index='0']").click()
     edit_task_1 = driver.find_element_by_id('com.ticktick.task:id/edit_text')
     edit_task_1.click()
     edit_task_1.send_keys('task_1_edited')
@@ -69,26 +62,33 @@ try:
     description.send_keys('item1')
     driver.find_element_by_id('com.ticktick.task:id/input_task_kind').click()
 
+
+    # ## edit tag.
+    # edit_tag = driver.find_element_by_id('com.ticktick.task:id/input_tag')
+    # edit_tag.click()
+    # edit_tag.send_keys('tag1')
+
+
+    driver.find_element_by_id('com.ticktick.task:id/input_close_keyboard').click()
     driver.find_element_by_xpath("//android.widget.ImageButton").click()
 except:
-    print('TC-07 error.')
+    print('TC-03 error.')
 
-# TC-08
+# TC-04
 try:
-## done task in calendar.
-    driver.find_element_by_id('com.ticktick.task:id/list').click()
-    driver.find_element_by_xpath("//android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v7.widget.RecyclerView/android.view.View[@index='2']").click()
+## done task.
+    driver.find_element_by_xpath("//android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.view.View[@index='0']").click()
     Checkbox = driver.find_element_by_id('com.ticktick.task:id/task_checkbox').click()
     driver.find_element_by_xpath("//android.widget.ImageButton").click()
 except:
-    print('TC-08 error.')
+    print('TC-04 error.')
 
-# TC-09
+# TC-05
 try:
-## delete task in calender.
-    driver.find_element_by_xpath("//android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v7.widget.RecyclerView/android.view.View[@index='2']").click()
+## delete task.
+    driver.find_element_by_xpath("//android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.view.View[@index='0']").click()
     tool_bar = driver.find_element_by_id('com.ticktick.task:id/toolbar').click()
     driver.find_element_by_xpath("//android.support.v7.widget.LinearLayoutCompat/android.widget.ImageView[@index='0']").click()
     driver.find_element_by_xpath("//android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[@index='9']").click()
 except:
-    print('TC-09 error.')
+    print('TC-05 error.')
